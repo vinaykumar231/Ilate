82153039039cee9de4a2b217afd3960893fead19
vinay <vinaymahto999@gmail.com>
2024-05-29 11:32:12 +0530

    29 may

--- Teacher_data/Employee/Model/__init__.py
+++
@@ -1,8 +0,0 @@
-# from ..Model.teacher_contact_info import TeacherContact
-# from ..Model.teacher_education import Education
-# from ..Model.teacher_skills import Skill
-# from ..Model.teacher_lang_knowledge import LanguagesSpoken
-# from ..Model.teacher_emergency_contact import EmergencyContact
-# from ..Model.teacher_depends import Dependents
-# from ..Model.employee_master_table import Employee
-# from ..Model.manager import Manager
